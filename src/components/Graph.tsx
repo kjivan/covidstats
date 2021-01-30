7dcece25634ee898f237bfe74c3ed107b44cd399
Kavi Jivan <kavi@jivan.cc>
2021-01-29 22:11:14 -0500

    Adds 7 day average blue line

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -6,9 +6,27 @@ export default class Graph extends Component<PropType, StateType> {
   state = {};
 
   render() {
+    const averageRange = 7;
     const data = {
       labels: this.props.dataLabels,
       datasets: [
+        {
+          type: "line",
+          fill: false,
+          borderColor: "#039BE5",
+          label: "average",
+          data: this.props.data.map((v, i, a) => {
+            if (i < (averageRange-1)) {
+              return 0;
+            } 
+            return (
+              a
+                .slice(i - (averageRange-1), i)
+                .reduce((a: number, v: ReactText) => a + (v as number), 0) /
+              averageRange
+            );
+          }),
+        },
         {
           label: this.props.label,
           data: this.props.data,
@@ -16,20 +34,20 @@ export default class Graph extends Component<PropType, StateType> {
           borderWidth: 1,
           borderColor: "#777",
           hoverBorderWidth: 3,
-          hoverBorderColor: "#000"
-        }
-      ]
+          hoverBorderColor: "#000",
+        },
+      ],
     };
     const options = {
       title: {
         display: true,
         text: this.props.title,
-        fontSize: 25
+        fontSize: 25,
       },
       legend: {
-        display: false
+        display: false,
       },
-      maintainAspectRatio: false
+      maintainAspectRatio: false,
     };
     return <Bar data={data} options={options} />;
   }
